ae19c5feac66c08a7d23a72d52ff7995283505ac
Sylvain Gizard <sylvain.gizard@gmail.com>
2014-11-26 15:25:04 +0100

    Fix svg-sprite crashing if no preserveFill option was present

--- svg-sprite/index.js
+++ svg-sprite/index.js
@@ -8,6 +8,8 @@ var SVG_WRAPPER = '<svg xmlns="http://www.w3.org/2000/svg">#shapes#</svg>';
 module.exports = function (options) {
     options = options || {};
 
+    var preserveFill = options.preserveFill || [];
+
     return function (tree) {
         var symbols = tree.nodes.reduce(function (acc, node) {
             var name = path.basename(node.name, '.svg');
@@ -22,7 +24,7 @@ module.exports = function (options) {
             shape = '<symbol ' + viewBox + ' id="' + name + '">' + shape + '</symbol>';
 
             // Remove fill attributes, in order to be able to style from CSS
-            if (options.preserveFill.indexOf(name) === -1) {
+            if (preserveFill.indexOf(name) === -1) {
                 shape = shape.replace(/\s*(?:fill|fill-opacity)="[^"]+"/g, '');
             }
 
