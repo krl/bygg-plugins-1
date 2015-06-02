c9190e5aca793977fa5e77a067d93d08ab6ed81b
Kristoffer Ström <kristoffer@rymdkoloni.se>
2015-06-02 17:05:11 +0200

    light

--- csswring/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var csswring = require('csswring');
-var postcss = require('../postcss');
-
-module.exports = function (options) {
-    return postcss([csswring(options)], 'csswring');
-};
